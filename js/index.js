4e98ba86d17e3729952b8dafb03173374d85a9f0
mollyyee <mollyyee1@Mollys-MacBook-Pro-3.local>
2017-09-03 15:44:26 -0700

    new portfolio

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+$('#popup1').mouseover(function() {
+	$('#colors').css('background-color', '#678286');
+})
+
+$('#popup2').mouseover(function() {
+	$('#colors').css('background-color', '#C1CEFF');
+})
+
+$('#name').mouseover(function() {
+	$('#colors').css('background-color', '#FFAA8B');
+})
+
+$('#popup3').mouseover(function() {
+	$('#colors').css('background-color', '#FFDA8A');
+})
+
+
+
+
+
+});
